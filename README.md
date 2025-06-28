b2f5e482e83beaab41a721ac0fd3bcac6d6b33a2
Dave Stearns <dave.stearns@gmail.com>
2025-06-28 11:23:04 -0700

    Typed application IDs example code

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # Application-Assigned IDs
+
+This repo contains example code discussed in my
+[application assigned IDs tutorial](https://davestearns.github.io/tutorials/ids.html).
+
+Specifically it contains a
+[base class](https://github.com/davestearns/ids/blob/main/base_id.py) you can
+use for defining strongly-typed and prefixed IDs in Python. For example:
+
+```python
+from base_id import BaseID
+
+class AccountID(BaseID):
+    PREFIX = "acct"
+
+class SessionID(BaseID):
+    PREFIX = "ses"
+
+account_id = AccountID()
+print(account_id) # => acct_3e6wlko0bp8e7c9916hklbnd
+
+# account_id is an AccountID
+assert type(account_id) is AccountID
+
+# But it's also just a string,
+# so it works anywhere a str does
+assert isinstance(account_id, str)
+
+# And SessionID is a different type than AccountID
+assert type(AccountID()) is not type(SessionID())
+
+# When you receive one of these IDs as a string,
+# you can rehydrate it back into the specific type
+# and this will raise ValueError if the ID isn't the
+# correct prefix.
+id_sent_to_client: str = str(account_id)
+rehydrated_account_id = AccountID(id_sent_to_client)
+assert type(rehydrated_account_id) is AccountID
+assert rehydrated_account_id == account_id
+
+# If you receive one of these IDs as a string, but
+# aren't sure what specific type it is, 
+# use BaseID.parse() to parse it into the appropriate
+# type based on its prefix. This will raise ValueError
+# if the prefix doesn't match any of the declared
+# prefixes on the various subclasses
+unknown_id_typ: str = str(account_id)
+parsed_id = BaseID.parse(unknown_id_type)
+assert type(parsed_id) is AccountID
+```
---
+++ base_id_test.py
@@ -0,0 +1,81 @@
+import pytest
+from .base_id import BaseID
+
+
+class AccountID(BaseID):
+    PREFIX = "acct"
+
+
+class SessionID(BaseID):
+    PREFIX = "ses"
+
+
+def test_creation() -> None:
+    assert str(AccountID()).startswith(AccountID.PREFIX + BaseID.PREFIX_SEPARATOR)
+    assert str(SessionID()).startswith(SessionID.PREFIX + BaseID.PREFIX_SEPARATOR)
+
+
+def test_equality() -> None:
+    id1 = AccountID()
+    id2 = id1
+    assert id2 == id1
+
+
+def test_inequality() -> None:
+    id1 = AccountID()
+    id2 = AccountID()
+    assert id1 != id2
+
+
+def test_ordering() -> None:
+    id1 = AccountID()
+    id2 = AccountID()
+    assert id2 >= id1
+    assert id1 <= id2
+
+
+def test_init() -> None:
+    id = AccountID()
+    rehydrated_id = AccountID(id)
+    assert type(rehydrated_id) is AccountID
+    assert rehydrated_id == id
+
+
+def test_init_fail() -> None:
+    acct_id = AccountID()
+    ses_id = SessionID()
+    with pytest.raises(ValueError):
+        SessionID(acct_id)
+    with pytest.raises(ValueError):
+        AccountID(ses_id)
+
+
+def test_parse() -> None:
+    id = AccountID()
+    parsed_id = BaseID.parse(id)
+    assert type(parsed_id) is AccountID
+    assert parsed_id == id
+
+
+def test_parse_fail() -> None:
+    with pytest.raises(ValueError):
+        BaseID.parse("invalid_123")
+
+
+def test_create_base_fail() -> None:
+    with pytest.raises(AttributeError):
+        BaseID()
+
+
+def test_define_duplicate_prefix_fail() -> None:
+    prefix = "test"
+    if prefix in BaseID.prefix_to_class_map:
+        del BaseID.prefix_to_class_map[prefix]
+
+    class TestID(BaseID):
+        PREFIX = prefix
+
+    with pytest.raises(ValueError):
+
+        class DuplicatePrefixID(BaseID):
+            PREFIX = prefix
